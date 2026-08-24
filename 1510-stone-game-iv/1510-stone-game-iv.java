d9e43017f1e6f876ca2aef307ca3a8aaf6bfa4f5
Sharad kumar <sharad.22scse1011832@galgotiasuniversity.edu.in>
2026-08-24 12:02:27 +0530

    Time: 2 ms (99.96%), Space: 42.6 MB (34.8%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -2,23 +2,16 @@ class Solution {
     public boolean winnerSquareGame(int n) {
         boolean[] dp = new boolean[n + 1];
 
-        // dp[0] = false
-        // No stones -> current player cannot move -> loses
-
-        for (int i = 1; i <= n; i++) {
-
-            // Try removing every possible square number
-            for (int j = 1; j * j <= i; j++) {
-
-                // If opponent reaches a losing state,
-                // current player can win.
-                if (!dp[i - j * j]) {
-                    dp[i] = true;
-                    break;
+        for (int i = 0; i <= n; i++) {
+            if (!dp[i]) {
+                for (int j = 1; i + j * j <= n; j++) {
+                    dp[i + j * j] = true;
+                }
+                if (dp[n]) {
+                    return true;
                 }
             }
         }
-
-        return dp[n];
+        return false;
     }
 }
